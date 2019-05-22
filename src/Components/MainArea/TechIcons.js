85d2c073dd3e0ea883a4a9538ae6b1d020bdcf37
aronedwards91 <aronedwards91@users.noreply.github.com>
2019-05-22 08:41:31 +0100

    skills placed

--- src/Components/MainArea/TechIcons.js
+++ src/Components/MainArea/TechIcons.js
@@ -11,15 +11,18 @@ class TechIcons extends React.PureComponent {
   render() {
     return (
       <div className="divideArea">
-        <div className="techIconsHeader" />
+        <div className="techIconsHeader">
+          <div className="text Bold" >Skill Ranking</div>
+        </div>
+
         <div className="techIcons">
-          <img src={agileImg} alt="Agile" className="iconBubble" />
-          <img src={expressImg} alt="Express" className="iconBubble" />
-          <img src={fastifyImg} alt="Fastify" className="iconBubble" />
-          <img src={githubImg} alt="Github" className="iconBubble" />
-          <img src={jsImg} alt="Js" className="iconBubble" />
-          <img src={linuxImg} alt="Linux" className="iconBubble" />
-          <img src={nodeImg} alt="Node" className="iconBubble" />
+          <img src={agileImg} alt="Agile" className="iconBubble skill4" />
+          <img src={expressImg} alt="Express" className="iconBubble skill3" />
+          <img src={fastifyImg} alt="Fastify" className="iconBubble skill7" />
+          <img src={githubImg} alt="Github" className="iconBubble skill5" />
+          <img src={jsImg} alt="Js" className="iconBubble skill1" />
+          <img src={linuxImg} alt="Linux" className="iconBubble skill6" />
+          <img src={nodeImg} alt="Node" className="iconBubble skill2" />
         </div>
       </div>
     );

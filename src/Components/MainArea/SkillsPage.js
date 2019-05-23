3fa7744f1ed27e0c269f3fff13c17266ba827914
aronedwards91 <aronedwards91@users.noreply.github.com>
2019-05-23 10:42:58 +0100

    built skills page basic outline

--- src/Components/MainArea/SkillsPage.js
+++ src/Components/MainArea/SkillsPage.js
@@ -1,10 +1,69 @@
 import React from "react";
+import strings from "../StringImporter";
+import jsImg from "../../res/icons/js.png";
+import "../../css/SkillsPage.css";
+
 
 class SkillsPage extends React.Component {
     render() {
         return (
-            <div>
-                TestSKillsPage
+            <div className="SkMain">
+                <div className="SkSkillArea">
+                    <img src={jsImg} alt="iconPic" className="SkSkillIcon"/>
+                    <div className="SkSkillTextBox">
+                        <div className="Text BigBold">{strings.skill1}</div>
+                        <div className="Text">{strings.skill1Summary}</div>
+                    </div>
+                </div>
+                <div className="SkSkillArea">
+                    <img src={jsImg} alt="iconPic" className="SkSkillIcon"/>
+                    <div className="SkSkillTextBox">
+                        <div className="Text BigBold">{strings.skill1}</div>
+                        <div className="Text">{strings.skill1Summary}</div>
+                    </div>
+                </div>
+                <div className="SkSkillArea">
+                    <img src={jsImg} alt="iconPic" className="SkSkillIcon"/>
+                    <div className="SkSkillTextBox">
+                        <div className="Text BigBold">{strings.skill1}</div>
+                        <div className="Text">{strings.skill1Summary}</div>
+                    </div>
+                </div>
+                <div className="SkSkillArea">
+                    <img src={jsImg} alt="iconPic" className="SkSkillIcon"/>
+                    <div className="SkSkillTextBox">
+                        <div className="Text BigBold">{strings.skill1}</div>
+                        <div className="Text">{strings.skill1Summary}</div>
+                    </div>
+                </div>
+                <div className="SkSkillArea">
+                    <img src={jsImg} alt="iconPic" className="SkSkillIcon"/>
+                    <div className="SkSkillTextBox">
+                        <div className="Text BigBold">{strings.skill1}</div>
+                        <div className="Text">{strings.skill1Summary}</div>
+                    </div>
+                </div>
+                <div className="SkSkillArea">
+                    <img src={jsImg} alt="iconPic" className="SkSkillIcon"/>
+                    <div className="SkSkillTextBox">
+                        <div className="Text BigBold">{strings.skill1}</div>
+                        <div className="Text">{strings.skill1Summary}</div>
+                    </div>
+                </div>
+                <div className="SkSkillArea">
+                    <img src={jsImg} alt="iconPic" className="SkSkillIcon"/>
+                    <div className="SkSkillTextBox">
+                        <div className="Text BigBold">{strings.skill1}</div>
+                        <div className="Text">{strings.skill1Summary}</div>
+                    </div>
+                </div>
+                <div className="SkSkillArea">
+                    <img src={jsImg} alt="iconPic" className="SkSkillIcon"/>
+                    <div className="SkSkillTextBox">
+                        <div className="Text BigBold">{strings.skill1}</div>
+                        <div className="Text">{strings.skill1Summary}</div>
+                    </div>
+                </div>
             </div>
         )
     }

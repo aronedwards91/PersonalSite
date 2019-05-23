e57bbc29d298ea4ff46bb0d732388ef379d8f189
aronedwards91 <aronedwards91@users.noreply.github.com>
2019-05-23 09:29:56 +0100

    implemented router

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import SideBar from "./SideBar/SideBar";
 import LandingPage from "./MainArea/LandingPage";
+import SkillsPage from "./MainArea/SkillsPage";
 import "../css/Vars.css";
 import "../css/Button.css";
 import "../css/Text.css";
@@ -8,14 +10,20 @@ import "../css/App.css";
 import "../css/SideBar.css";
 import "../css/LandingPage.css";
 
-
 class App extends Component {
   render() {
     return (
-      <div className="Main">
-        <SideBar />
-        <LandingPage/>
-      </div>
+      <BrowserRouter>
+        <div className="Main">
+          <SideBar />
+          <main>
+            <switch>
+              <Route path="/" exact component={LandingPage} />
+              <Route path="/skills" component={SkillsPage} />
+            </switch>
+          </main>
+        </div>
+      </BrowserRouter>
     );
   }
 }

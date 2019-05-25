67fd9302ca4cd654a312b4650304f7bb4842e158
aronedwards91 <aronedwards91@users.noreply.github.com>
2019-05-25 14:02:27 +0100

    added images to project page

--- src/Components/MainArea/ProjectPage.js
+++ src/Components/MainArea/ProjectPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import strings from "../StringImporter";
+import res from "../resImporter";
 import "../../css/ProjectBoxPB.css";
 
 class ProjectPage extends React.Component {
@@ -12,50 +13,74 @@ class ProjectPage extends React.Component {
         <div className="projectBoxPB">
           <div className="divideBoxTextPB">
             <div className="Text TitleBox">{strings.proj1}</div>
-            <div className="Text Bold smallPadding">{strings.proj1TechStack}</div>
+            <div className="Text Bold smallPadding">
+              {strings.proj1TechStack}
+            </div>
             <div className="Text smallPadding">{strings.proj1Summary}</div>
           </div>
-          <div className="divideBoxImgPB" />
+          <div className="divideBoxImgPB">
+            <img src={res.proj.project1} alt="proj1" className="projectImgPB" />
+          </div>
         </div>
         <div className="projectBoxPB">
           <div className="divideBoxTextPB">
             <div className="Text TitleBox">{strings.proj2}</div>
-            <div className="Text Bold smallPadding">{strings.proj2TechStack}</div>
+            <div className="Text Bold smallPadding">
+              {strings.proj2TechStack}
+            </div>
             <div className="Text smallPadding">{strings.proj2Summary}</div>
           </div>
-          <div className="divideBoxImgPB" />
+          <div className="divideBoxImgPB">
+            <img src={res.proj.project2} alt="proj2" className="projectImgPB" />
+          </div>
         </div>
         <div className="projectBoxPB">
           <div className="divideBoxTextPB">
             <div className="Text TitleBox">{strings.proj3}</div>
-            <div className="Text Bold smallPadding">{strings.proj3TechStack}</div>
+            <div className="Text Bold smallPadding">
+              {strings.proj3TechStack}
+            </div>
             <div className="Text smallPadding">{strings.proj3Summary}</div>
           </div>
-          <div className="divideBoxImgPB" />
+          <div className="divideBoxImgPB">
+            <img src={res.proj.project3} alt="proj3" className="projectImgPB" />
+          </div>
         </div>
         <div className="projectBoxPB">
           <div className="divideBoxTextPB">
             <div className="Text TitleBox">{strings.proj4}</div>
-            <div className="Text Bold smallPadding">{strings.proj4TechStack}</div>
+            <div className="Text Bold smallPadding">
+              {strings.proj4TechStack}
+            </div>
             <div className="Text smallPadding">{strings.proj4Summary}</div>
           </div>
-          <div className="divideBoxImgPB" />
+          <div className="divideBoxImgPB">
+            <img src={res.proj.project4} alt="proj4" className="projectImgPB" />
+          </div>
         </div>
         <div className="projectBoxPB">
           <div className="divideBoxTextPB">
             <div className="Text TitleBox">{strings.proj5}</div>
-            <div className="Text Bold smallPadding">{strings.proj5TechStack}</div>
+            <div className="Text Bold smallPadding">
+              {strings.proj5TechStack}
+            </div>
             <div className="Text smallPadding">{strings.proj5Summary}</div>
           </div>
-          <div className="divideBoxImgPB" />
+          <div className="divideBoxImgPB">
+            <img src={res.proj.project5} alt="proj5" className="projectImgPB" />
+          </div>
         </div>
         <div className="projectBoxPB">
           <div className="divideBoxTextPB">
             <div className="Text TitleBox">{strings.proj6}</div>
-            <div className="Text Bold smallPadding">{strings.proj6TechStack}</div>
+            <div className="Text Bold smallPadding">
+              {strings.proj6TechStack}
+            </div>
             <div className="Text smallPadding">{strings.proj6Summary}</div>
           </div>
-          <div className="divideBoxImgPB" />
+          <div className="divideBoxImgPB">
+            <img src={res.proj.project6} alt="proj6" className="projectImgPB" />
+          </div>
         </div>
       </div>
     );
--- src/Components/resImporter.js
+++ src/Components/resImporter.js
@@ -1,11 +1,20 @@
 //private Imports - control by delete or commenting out
 import headPic from "../res/headsmallPriv.png";
+
 import Biz1 from "../res/BusinessIconsPriv/Business1.png";
 import Biz2 from "../res/BusinessIconsPriv/Business2.png";
 import Biz3 from "../res/BusinessIconsPriv/Business3.png";
 import Biz4 from "../res/BusinessIconsPriv/Business4.png";
 import Biz5 from "../res/BusinessIconsPriv/Business5.png";
 import Biz6 from "../res/BusinessIconsPriv/Business6.png";
+
+import proj1 from "../res/projImgsPriv/proj1.png";
+import proj2 from "../res/projImgsPriv/proj2.png";
+import proj3 from "../res/projImgsPriv/proj3.png";
+import proj4 from "../res/projImgsPriv/proj4.png";
+import proj5 from "../res/projImgsPriv/proj5.png";
+import proj6 from "../res/projImgsPriv/proj6.png";
+
 //public Imports
 /**
 import headPic from "../res/headsmall.png";
@@ -15,6 +24,13 @@ import Biz3 from "../res/BusinessIcons/Business3.png";
 import Biz4 from "../res/BusinessIcons/Business4.png";
 import Biz5 from "../res/BusinessIcons/Business5.png";
 import Biz6 from "../res/BusinessIcons/Business6.png";
+
+import proj1 from "../res/projImgs/proj1.png";
+import proj2 from "../res/projImgs/proj2.png";
+import proj3 from "../res/projImgs/proj3.png";
+import proj4 from "../res/projImgs/proj4.png";
+import proj5 from "../res/projImgs/proj5.png";
+import proj6 from "../res/projImgs/proj6.png";
 */
 var res = {
     head: headPic,
@@ -25,6 +41,14 @@ var res = {
         Business4: Biz4,
         Business5: Biz5,
         Business6: Biz6
+    },
+    proj: {
+        project1: proj1,
+        project2: proj2,
+        project3: proj3,
+        project4: proj4,
+        project5: proj5,
+        project6: proj6
     }
 }
 
